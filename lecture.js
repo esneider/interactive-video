d293ef1cd67f80037c81eaaab742846af036547e
esneider <dariosn@gmail.com>
2014-07-08 03:19:53 -0300

    Fix a couple of bugs

--- lecture.js
+++ lecture.js
@@ -19,7 +19,6 @@ var Lecture = (function(document) {
         video: {
             controls: true,
             autoplay: false,
-            crossOrigin: false,
             currentTime: 0,
             defaultPlaybackRate: 1,
             loop: false,
@@ -81,7 +80,47 @@ var Lecture = (function(document) {
     }
 
     /**
+     * Create a new video HTML element.
+     *
+     * @param {string} id - Video element id.
+     * @param {string} uri - Video uri.
+     * @param {object} options - Video configuration options.
+     * @return {object} New video element
+     */
+    function newVideoHTML(id, uri, options) {
+
+        var video = document.createElement('video');
+
+        video.setAttribute('id', id);
+        video.style.display = 'none';
+        video.style.position = 'fixed';
+
+        var source = document.createElement('source');
+
+        source.setAttribute('src', uri);
+        source.setAttribute('type', 'video/mp4');
+
+        var text = document.createTextNode('Your browser does not html5 video');
+
+        video.appendChild(source);
+        video.appendChild(text);
+
+        for (var attr in options) {
+            if (options.hasOwnProperty(attr)) {
+                switch (typeof options[attr]) {
+                    case 'object':  break;
+                    case 'boolean': if (!options[attr]) break; // fallthrough
+                    default:        video.setAttribute(attr, options[attr]);
+                }
+            }
+        }
+
+        return video;
+    }
+
+    /**
      * Add a new video fragment.
+     *
      * TODO: add support for multiple video sources/formats.
      *
      * @memberof Lecture
@@ -96,28 +135,7 @@ var Lecture = (function(document) {
         extend(options, this.options.video);
         extend(options, this.options.general);
 
-        var video = document.createElement('video');
-
-        video.setAttribute('id', id);
-        video.style.display = 'none';
-        video.style.position = 'fixed';
-
-        for (var attr in options) {
-            if (options.hasOwnProperty(attr) && typeof options[attr] !== 'object') {
-                video.setAttribute(attr, options[attr]);
-            }
-        }
-
-        var source = document.createElement('source');
-
-        source.setAttribute('src', uri);
-        source.setAttribute('type', 'video/mp4');
-
-        var text = document.createTextNode('Your browser does not html5 video');
-
-        video.appendChild(source);
-        video.appendChild(text);
-
+        var video = newVideoHTML(id, uri, options);
         this.element.appendChild(video);
         this.videos[id] = options;
 
@@ -157,30 +175,29 @@ var Lecture = (function(document) {
      * Create a new XMLHttpRequest.
      */
     function newHttpRequest() {
-        try {return new XMLHttpRequest();}
-        catch (error) {}
-        try {return new ActiveXObject("Msxml2.XMLHTTP");}
-        catch (error) {}
-        try {return new ActiveXObject("Microsoft.XMLHTTP");}
-        catch (error) {}
+        try {
+            return new XMLHttpRequest();
+        } catch (error) {}
 
-        throw new Error("Could not create HTTP request object.");
+        try {
+            return new ActiveXObject('Msxml2.XMLHTTP');
+        } catch (error) {}
+
+        try {
+            return new ActiveXObject('Microsoft.XMLHTTP');
+        } catch (error) {}
+
+        throw new Error('Could not create HTTP request object.');
     }
 
     /**
-     * Add a new overlay.
-     *
-     * @memberof Lecture
+     * Create a new overlay HTML element.
      *
      * @param {string} id - Overlay element id.
      * @param {string} uri - Overlay uri.
      * @param {object} options - Overlay configuration options.
      */
-    Lecture.prototype.addOverlay = function(id, uri, options) {
-
-        options = options || {};
-        extend(options, this.options.overlay);
-        extend(options, this.options.general);
+    function newOverlayHTML(id, uri, options) {
 
         var overlay = document.createElement('div');
 
@@ -208,15 +225,35 @@ var Lecture = (function(document) {
         overlay.appendChild(background);
         overlay.appendChild(foreground);
 
+        return overlay;
+    }
+
+    /**
+     * Add a new overlay.
+     *
+     * @memberof Lecture
+     *
+     * @param {string} id - Overlay element id.
+     * @param {string} uri - Overlay uri.
+     * @param {object} options - Overlay configuration options.
+     */
+    Lecture.prototype.addOverlay = function(id, uri, options) {
+
+        options = options || {};
+        extend(options, this.options.overlay);
+        extend(options, this.options.general);
+
+        var overlay = newOverlayHTML(id, uri, options);
         this.element.appendChild(overlay);
         this.overlays[id] = options;
 
         var request = newHttpRequest();
-        request.open("GET", uri, true);
+        request.open('GET', uri, true);
         request.send();
         request.onreadystatechange = function() {
             if (request.readyState == 4) {
                 if (request.status == 200 || request.status == 304) {
+                    var foreground = overlay.getElementsByName('div')[1];
                     foreground.innerHTML = request.responseText;
                 }
             }
